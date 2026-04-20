d81d13cf3d6869cbae49f05cfe7568f1c8175193
Taman Patel <taman.hansalia@gmail.com>
2026-04-20 11:06:36 +0530

    package fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Toast } from "@heroui/react";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Toast.Provider placement="top end" />
     <App />
   </StrictMode>
 );
